ac9cdf2e8758dcbb6badebff6a81c89f6b788a32
ChatDisabled <44729807+ChatDisabled@users.noreply.github.com>
2023-06-16 05:19:47 +0200

    chore: base resource template

---
+++ README.md
@@ -0,0 +1,3 @@
+<p align="center">
+  <img src="https://i.imgur.com/YTHfNGG.gif" alt="pog">
+</p>
---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+version '1.0.0'
+repository 'https://github.com/Qbox-project/qbx-properties'
+description 'Hopefully one day a feature rich property system'
+
+shared_script 'config.lua'
+client_script 'client.lua'
+server_script 'server.lua'
+
+lua54 'yes'
+use_experimental_fxv2_oal 'yes'
