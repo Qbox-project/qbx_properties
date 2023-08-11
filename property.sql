249a0f263e02dde76d62e523e5f10a6e0e3ed4d8
Tony <97451137+TonybynMp4@users.noreply.github.com>
2023-08-11 02:39:53 +0200

    fix SQL (mariadb doesn't like me)

--- property.sql
+++ property.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS `properties` (
 	`rent_date` TIMESTAMP DEFAULT NULL,
 	`stash` JSON,
 	`outfit` JSON,
-	`logout` JSOn,
+	`logout` JSON,
 	`decorationsid` INT,
 	`appliedtaxes` JSON,
 	`maxweight` INT,
@@ -20,15 +20,17 @@ CREATE TABLE IF NOT EXISTS `properties` (
 
 CREATE TABLE IF NOT EXISTS `property_owners` (
 	`property_id` INT NOT NULL,
-	`citizenid` INT NOT NULL,
+	`citizenid` VARCHAR(50) NOT NULL, /* CHANGE WHEN DB REFACTOR GOES THROUGH (cmon illenium it's time) */
 	`role` ENUM('owner', 'co_owner', 'tenant') NOT NULL,
-	PRIMARY KEY (property_id, citizenid) ON DELETE CASCADE ON UPDATE CASCADE
+	PRIMARY KEY (property_id, citizenid),
+	FOREIGN KEY (property_id) REFERENCES `properties` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+	FOREIGN KEY (citizenid) REFERENCES `players` (`citizenid`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS `property_decorations` (
-	`id` int(11) NOT NULL AUTO_INCREMENT,
-	`name` varchar(255) NOT NULL,
-	`price` int(12) NOT NULL,
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(255) NOT NULL,
+	`price` INT NOT NULL,
 	`decorations` JSON NOT NULL,
 	PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+);
