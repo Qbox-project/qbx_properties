13f81c890e59cb54ec5affeecfcf9b2a5c82a7fb
stepanvalic <valicekstepan8@seznam.cz>
2023-10-14 21:09:02 +0200

    Czech translation created

---
+++ locales/cs.lua
@@ -0,0 +1,17 @@
+local Translations = {
+    success = {
+    },
+    error = {
+    },
+    general = {
+    },
+}
+
+if GetConvar('qb_locale', 'en') == 'cs' then
+    Lang = Locale:new({
+        phrases = Translations,
+        warnOnMissing = true,
+        fallbackLang = Lang,
+    })
+end
+--translate by stepan_valic
