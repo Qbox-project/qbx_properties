4cbfc0b3cb41cea1f7403b3bc77c4106d452a96f
ashleyjrobinson <ashjrobinson95@gmail.com>
2023-12-12 18:34:29 +0000

    SQL Syntax Error

--- property.sql
+++ property.sql
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS `property_owners` (
 	`citizenid` VARCHAR(50) NOT NULL, /* CHANGE WHEN DB REFACTOR GOES THROUGH (cmon illenium it's time) */
 	`role` ENUM('owner', 'co_owner', 'tenant') NOT NULL,
 	PRIMARY KEY (property_id, citizenid),
-	FOREIGN KEY (property_id) REFERENCES `properties` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+	FOREIGN KEY (property_id) REFERENCES `properties` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
 	FOREIGN KEY (citizenid) REFERENCES `players` (`citizenid`) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
