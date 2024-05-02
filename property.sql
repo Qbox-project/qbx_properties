130b04cc2da2c27bd9bbf7af764bca254bd72cce
ChatDisabled <44729807+ChatDisabled@users.noreply.github.com>
2024-05-02 15:59:46 +0200

    chore(sql): remove foreign key
    Wanted to do something fun with it, not anymore :salute:

--- property.sql
+++ property.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `properties` (
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`property_name` VARCHAR(255) NOT NULL, /* simple label that can be used to identify which property it is. I.e. used for spawn list */
-	`coords` JSON NOT NULL,
+	`coords` JSON NOT NULL, /* vector3 world coords for the entry position */
 	`price` INT NOT NULL DEFAULT 0,
 	`owner` VARCHAR(255), /* citizen ID of the owner */
 	`interior` VARCHAR(255) NOT NULL, /* the interior name, can range from IPL name to a shell hash that needs to spawn in */
@@ -9,7 +9,6 @@ CREATE TABLE IF NOT EXISTS `properties` (
 	`rent_interval` INT DEFAULT NULL, /* the rent interval in hours */
 	`interact_options` JSON NOT NULL DEFAULT (JSON_OBJECT()), /* clothing and exit points */
 	`stash_options` JSON NOT NULL DEFAULT (JSON_OBJECT()), /* multiple stash support */
-	`decorations` JSON NOT NULL DEFAULT (JSON_OBJECT()), /* the model name with it's corresponding coords that needs to be placed */
-	FOREIGN KEY (owner) REFERENCES `players` (`citizenid`),
+	`decorations` JSON NOT NULL DEFAULT (JSON_OBJECT()), /* the model name with its corresponding coords that needs to be placed */
 	PRIMARY KEY (id)
 );
